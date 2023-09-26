a99892e514f1a6802a8ef3f737c04c45bd35c6cb
strojn <anatolii.strojn@gmail.com>
2023-09-26 15:53:44 +0300

    1st

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-import java.util.Scanner;
+//Створіть проект з класом Main.java.
+//
+//        Потрібно: Створити клас Book. Створити класи Title, Author та Content,
+//        кожен з яких повинен містити одне рядкове поле та метод void show().
+//        Реалізувати можливість додавання назви книги, імені автора та змісту.
+//        Виведіть на екран за допомогою методу show() назву книги, ім'я автора та Зміст.
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
